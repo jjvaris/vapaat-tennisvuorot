b61293a267cb7bad8e2850928961ab12a998c7ba
Jouni Varis <jjvaris@gmail.com>
2020-03-12 20:02:42 +0200

    add smash center helsinki

--- src/hallit/index.js
+++ src/hallit/index.js
@@ -2,6 +2,7 @@ import tali from './talintenniskeskus';
 import meilahti from './meilahti';
 import taivallahti from './taivallahti';
 import esport from './esportTapiola';
+import smash from './smash';
 
 const getAllAvailableHours = async date =>
   await Promise.all([
@@ -9,7 +10,8 @@ const getAllAvailableHours = async date =>
     meilahti(date),
     taivallahti(date),
     esport.esport1(date),
-    esport.esport2(date)
+    esport.esport2(date),
+    smash(date)
   ]);
 
 export default getAllAvailableHours;
---
+++ src/hallit/smash.js
@@ -0,0 +1,31 @@
+//https://smashcenter.slsystems.fi/booking/booking-calendar?BookingCalForm%5Bp_laji%5D=1&BookingCalForm%5Bp_pvm%5D=2020-03-12&BookingCalForm%5Bp_pvm_interval%5D=&BookingCalForm%5Bp_calmode%5D=2&BookingCalForm%5Bp_pvm_custom%5D=Torstai+12.03.2020
+import { get } from '../utils/api';
+import $ from 'jquery';
+import { format } from 'date-fns';
+
+const getAvailableHours = async date => {
+  const today = format(date, 'yyyy-MM-dd');
+  const url = `https://smashcenter.slsystems.fi/booking/booking-calendar?BookingCalForm%5Bp_laji%5D=1&BookingCalForm%5Bp_pvm%5D=${today}&BookingCalForm%5Bp_pvm_interval%5D=&BookingCalForm%5Bp_calmode%5D=2&BookingCalForm%5Bp_pvm_custom%5D=Torstai+12.03.2020`;
+  const response = await get(url);
+  const html = response.data.contents;
+  const availableHours = $(html)
+    .find('td[class="s-avail"]')
+    .map((_, e) => {
+      return $(e)
+        .text()
+        .trim();
+    })
+    .get()
+    .filter(hour => hour.startsWith('K'))
+    .map(hour => {
+      const s = hour.split(':');
+      return `${s[0].substring(s[0].length - 2)}:${s[1].substring(0, 2)}`;
+    });
+  return {
+    name: 'Smash Center Helsinki',
+    availableHours: [...new Set(availableHours)],
+    link: url
+  };
+};
+
+export default getAvailableHours;
