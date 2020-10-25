1bf8bd40e61f964e4e128b2007ad1eed70a1d555
Jouni Varis <jjvaris@gmail.com>
2020-10-25 15:48:49 +0200

    filter out smash center helsinki k14 outdoor court

--- src/hallit/smashHelsinki.js
+++ src/hallit/smashHelsinki.js
@@ -2,7 +2,7 @@ import { get } from '../utils/api';
 import $ from 'jquery';
 import { format } from 'date-fns';
 
-const getAvailableHours = async date => {
+const getAvailableHours = async (date) => {
   const today = format(date, 'yyyy-MM-dd');
   const url = `https://smashcenter.slsystems.fi/booking/booking-calendar?BookingCalForm%5Bp_laji%5D=1&BookingCalForm%5Bp_pvm%5D=${today}&BookingCalForm%5Bp_pvm_interval%5D=&BookingCalForm%5Bp_calmode%5D=2&BookingCalForm%5Bp_pvm_custom%5D=Torstai+12.03.2020`;
   const response = await get(url);
@@ -10,20 +10,19 @@ const getAvailableHours = async date => {
   const availableHours = $($.parseHTML(html))
     .find('td[class="s-avail"]')
     .map((_, e) => {
-      return $(e)
-        .text()
-        .trim();
+      return $(e).text().trim();
     })
     .get()
-    .filter(hour => hour.startsWith('K'))
-    .map(hour => {
+    .filter((hour) => hour.startsWith('K'))
+    .filter((hour) => !hour.startsWith('K14'))
+    .map((hour) => {
       const s = hour.split(':');
       return `${s[0].substring(s[0].length - 2)}:${s[1].substring(0, 2)}`;
     });
   return {
     name: 'Smash Center Helsinki',
     availableHours: [...new Set(availableHours)],
-    link: url
+    link: url,
   };
 };
 
