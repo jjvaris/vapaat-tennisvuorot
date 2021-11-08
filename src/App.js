6591711727e46ede3cb5e4a7446d1682a6f75090
Jouni Varis <jjvaris@gmail.com>
2021-11-08 23:36:18 +0200

    fetch available hours from backend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import React from 'react';
-import getAllAvailableHours from './hallit';
 import { format, parseISO } from 'date-fns';
 import DatePicker from 'react-datepicker';
 import TimeRangePicker from './components/TimeRangePicker';
 import { Box, Heading } from '@chakra-ui/core';
+
+import { client } from './utils/api';
 
 const urlParams = new URLSearchParams(window.location.search);
 const startUrlParam = urlParams.get('start');
 const endUrlParam = urlParams.get('end');
 const dateUrlParam = urlParams.get('date') && parseISO(urlParams.get('date'));
 
-const formatDate = date => {
+const formatDate = (date) => {
   return format(date, 'yyyy-MM-dd');
 };
 
-const resolveSearchDate = date => {
+const resolveSearchDate = (date) => {
   const today = new Date();
   if (!date) {
     return today;
@@ -23,23 +24,83 @@ const resolveSearchDate = date => {
   return date < today ? today : date;
 };
 
+const getHours = (data, searchDate) => {
+  if (!data) {
+    return [];
+  }
+
+  const byDay = data.availableHours.reduce((acc, cur) => {
+    if (!acc[cur.day]) {
+      acc[cur.day] = [cur];
+      return acc;
+    }
+    acc[cur.day] = [...acc[cur.day], cur];
+    return acc;
+  }, {});
+
+  const allByDay = byDay[format(searchDate, 'yyyy-MM-dd')];
+
+  if (!allByDay) {
+    return [];
+  }
+
+  const byHallId = allByDay
+    .filter((i) => i.courtType === 'INSIDE' && !i.thirtyMinutes)
+    .reduce((acc, cur) => {
+      if (!acc[cur.hallId]) {
+        acc[cur.hallId] = {
+          name: data.halls.find((hall) => hall.id === cur.hallId).name,
+          availableHours: [cur.hour],
+          link: cur.link,
+        };
+        return acc;
+      }
+      acc[cur.hallId].availableHours = [
+        ...new Set([...acc[cur.hallId].availableHours, cur.hour]),
+      ];
+      return acc;
+    }, {});
+
+  return data.halls
+    .slice()
+    .sort((a, b) => {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    })
+    .map((hall) => ({
+      name: hall.name,
+      availableHours: byHallId[hall.id]?.availableHours ?? [],
+      link: byHallId[hall.id]?.link ?? hall.link,
+    }));
+};
+
 function App() {
+  const [data, setData] = React.useState();
   const [searchDate, setSearchDate] = React.useState(
     resolveSearchDate(dateUrlParam)
   );
   const [[start, end], setTimeRange] = React.useState([
     startUrlParam || '06:00',
-    endUrlParam || '22:00'
+    endUrlParam || '22:00',
   ]);
   const [places, setPlaces] = React.useState([]);
 
   React.useEffect(() => {
-    async function fetchAvailableHours(date) {
-      setPlaces(await getAllAvailableHours(date));
-    }
+    client
+      .get('https://vapaat-pelivuorot.fly.dev/api/available-hours')
+      .then((response) => setData(response.data));
+  }, []);
 
-    fetchAvailableHours(searchDate);
-  }, [setPlaces, searchDate]);
+  React.useEffect(() => {
+    setPlaces(getHours(data, searchDate));
+  }, [setPlaces, searchDate, data]);
 
   React.useEffect(() => {
     window.history.pushState(
@@ -52,7 +113,7 @@ function App() {
   }, [searchDate, start, end]);
 
   const handleTimeRangeChange = React.useCallback(
-    range => setTimeRange(range),
+    (range) => setTimeRange(range),
     []
   );
 
@@ -61,7 +122,7 @@ function App() {
       <Heading mb="2rem">Vapaat tennisvuorot</Heading>
       <DatePicker
         selected={searchDate}
-        onChange={date => {
+        onChange={(date) => {
           setSearchDate(date);
           setPlaces([]);
         }}
@@ -70,7 +131,7 @@ function App() {
       />
       <TimeRangePicker onChange={handleTimeRangeChange} from={start} to={end} />
       {places.length > 0 ? (
-        places.map(place => (
+        places.map((place) => (
           <div key={place.name}>
             <a href={place.link} target="_blank" rel="noopener noreferrer">
               <Heading size="sm" my="1rem">
@@ -87,7 +148,11 @@ function App() {
         ))
       ) : (
         <div style={{ marginTop: '20px' }}>
-          <h2>Ladataan vapaita vuoroja...</h2>
+          <h2>
+            {!data
+              ? 'Ladataan vapaita vuoroja...'
+              : 'Valittu päivämäärä ylittää kaksi viikkoa nykyhetkestä, katso vapaat ajat hallien sivuilta'}
+          </h2>
         </div>
       )}
     </div>
@@ -97,10 +162,10 @@ function App() {
 function renderHours(place, start, end) {
   const hours = place.availableHours
     .filter(
-      hour =>
+      (hour) =>
         parseInt(hour) >= parseInt(start) && parseInt(hour) <= parseInt(end)
     )
-    .map(hour => (
+    .map((hour) => (
       <a href={place.link} key={hour} target="_blank" rel="noopener noreferrer">
         <span
           style={{
@@ -108,7 +173,7 @@ function renderHours(place, start, end) {
             margin: '5px',
             border: 'solid',
             borderWidth: '1px',
-            padding: '3px'
+            padding: '3px',
           }}
         >
           {hour}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const client = axios.create({ timeout: 5000 });
+export const client = axios.create({ timeout: 5000 });
 
-export const get = url =>
+export const get = (url) =>
   client.get(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
